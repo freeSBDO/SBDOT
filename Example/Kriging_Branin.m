2125e978c09d3dc274dd57e6db8150cff931efad
DURANTIN Cédric 243710 <cedric.durantin@cea.fr>
2017-06-28 17:01:25 +0200

    Normalize data before theta_bound computation.
    Update Branin example with rmse on test points.

--- Example/Kriging_Branin.m
+++ Example/Kriging_Branin.m
@@ -28,6 +28,11 @@ krig.Plot( [1,2], [] )
 % Plot output depending on the input_1 , with input_2 = 5
 krig.Plot( 1, 5 )
 
+x_test = stk_sampling_maximinlhs( 100, 2, [lb; ub], 200);
+x_test = x_test.data;
+y_test = Branin( x_test );
+rmse = krig.Rmse( x_test, y_test );
+
 %%
 % Create Kriging for regression
 krig_reg = Kriging ( prob , 1 , [] ,'reg', true );
--- Src/Tools/Theta_bound.m
+++ Src/Tools/Theta_bound.m
@@ -6,6 +6,13 @@ function [ mean_theta, lb_theta, ub_theta ] = Theta_bound( x )
 % Syntax :
 % [mean_theta,lb_theta,ub_theta]=Theta_bound(x);
 
+
+%% Normalize x
+n = size( x, 1);
+inputAvg = mean( x );
+inputStd = std( x );
+x = (x - inputAvg(ones(n,1),:)) ./ inputStd(ones(n,1),:);
+
 % Manhattan Distance between input points calculation
 x_2 = permute(x',[3 1 2]);
 dist(:,:)=sum(abs(bsxfun(@minus,x,x_2)),2); 
