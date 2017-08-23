7e0f235e0e8e4767810701432bdbd3d3780e93c1
DURANTIN Cédric 243710 <cedric.durantin@cea.fr>
2017-08-23 16:29:34 +0200

    Expected improvement allows to optimize using qualitative values

--- Src/Adaptive_Sampling/@Expected_improvement/EI_constrained.m
+++ Src/Adaptive_Sampling/@Expected_improvement/EI_constrained.m
@@ -6,10 +6,20 @@ function [ EI_val, cons ] = EI_constrained( obj, x_test )
 % for constrained case at x_test
 
 % Prediction value and error at test point
-[ y_pred, MSE_pred ] = obj.meta_y.Predict( x_test );
-
-for i = 1 : obj.m_g
-    [g_pred(:,i), MSE_g_pred(:,i) ] = obj.meta_g(i,:).Predict(x_test);
+if isequal(obj.meta_type,@Q_kriging)
+    n_test = size(x_test,1);
+    [ y_pred, MSE_pred ] = obj.meta_y.Predict( x_test,repmat(obj.QV_val,n_test,1) );
+    
+    for i = 1 : obj.m_g
+        [g_pred(:,i), MSE_g_pred(:,i) ] = obj.meta_g(i,:).Predict(x_test,repmat(obj.QV_val,n_test,1));
+    end
+else
+    
+    [ y_pred, MSE_pred ] = obj.meta_y.Predict( x_test );
+    
+    for i = 1 : obj.m_g
+        [g_pred(:,i), MSE_g_pred(:,i) ] = obj.meta_g(i,:).Predict(x_test);
+    end
 end
 
 EI1 = stk_sampcrit_ei_eval(y_pred, sqrt(abs( MSE_pred )), obj.y_min);
--- Src/Adaptive_Sampling/@Expected_improvement/EI_unconstrained.m
+++ Src/Adaptive_Sampling/@Expected_improvement/EI_unconstrained.m
@@ -1,10 +1,14 @@
 function [ EI_val ] = EI_unconstrained( obj, x_test )
- %EI_UNCONSTRAINED
+%EI_UNCONSTRAINED
 % Compute the Expected Improvement value for unconstrained case at x_test
 
 % Prediction value and error at test point
-[ y_pred, MSE_pred ] = obj.meta_y.Predict( x_test );
-
+if isequal(obj.meta_type,@Q_kriging)
+    n_test = size(x_test,1);
+    [ y_pred, MSE_pred ] = obj.meta_y.Predict( x_test,repmat(obj.QV_val,n_test,1) );
+else
+    [ y_pred, MSE_pred ] = obj.meta_y.Predict( x_test );
+end
 EI_val = -stk_sampcrit_ei_eval(y_pred, sqrt(abs( MSE_pred )), obj.y_min);
 
 end
